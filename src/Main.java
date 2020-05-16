4fbecc78f29af94f8d92fbcc93e1ab43ef20406a
surgutov <surgutovpersonal@gmail.com>
2020-05-16 21:18:44 +0300

    initialo commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        float price = (float) 2990;
+        int charge = 20;
+        int miles = (int) (price / charge);
+        System.out.print(miles);
+    }
+}
